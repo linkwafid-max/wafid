f32804663430c1ea2f51802eed38292d06c000dd
linkwafid-max <linkwafid@gmail.com>
2025-11-16 00:25:48 +0600

    Remove validation time and payment link
    Removed validation time and payment link from script.

--- wafid_link_scripts.js
+++ wafid_link_scripts.js
@@ -1,4 +1,4 @@
 Username: test.admin432                      Total balance: 128.37
                           System Offline
-Validation time is 5 minutes. (11:56 PM)
-https://wafid.com/appointment/2WX65Y92BNJ5d1N/pay/
+
+No link
