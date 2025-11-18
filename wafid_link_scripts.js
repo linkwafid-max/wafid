af3dba47409e0eb36b0200ce7a461f98729cfddf
linkwafid-max <linkwafid@gmail.com>
2025-11-19 03:03:35 +0600

    Update wafid_link_scripts.js

--- wafid_link_scripts.js
+++ wafid_link_scripts.js
@@ -1,3 +1,4 @@
 Username: test.admin432                      Total balance: 128.37
-System Offline      The system will only be online for 20 minutes from (11:03 to 11:25).
+                          System Offline 
+                          Wait for link
 No link
