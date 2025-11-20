2c64eba094459ed179dc221328583811d279b9ef
linkwafid-max <linkwafid@gmail.com>
2025-11-21 01:15:38 +0600

    Update wafid_link_scripts.js with appointment links
    Added appointment links and validation time information.

--- wafid_link_scripts.js
+++ wafid_link_scripts.js
@@ -1,4 +1,6 @@
 Username: test.admin432                      Total balance: 128.37
                           System Offline 
-                          Wait for link
-No link
+Time: 01:13 AM
+https://wafid.com/appointment/pPavnB9AAPGeNrL/pay/ (Validation time only 5 minutes)
+https://wafid.com/appointment/POE95bkPP8JeQaA/pay/ (Validation time only 5 minutes)
+https://wafid.com/appointment/VP9p5LWppKj4dzm/pay/ (Validation time only 5 minutes)
