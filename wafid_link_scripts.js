705727b3cb820eb3c6ed68377bb172c085ba871c
linkwafid-max <linkwafid@gmail.com>
2026-05-06 18:34:09 +0600

    Delete wafid_link_scripts.js

--- wafid_link_scripts.js
+++
@@ -1,6 +0,0 @@
-Username: test.admin432                      Total balance: -13.63
-                          System Offline 
-Time: 05:48 PM
-https://wafid.com/appointment/pPavnB9AAPGeNrL/pay/ (Validation time only 6 minutes)
-https://wafid.com/appointment/POE95bkPP8JeQaA/pay/ (Validation time only 6 minutes)
-https://wafid.com/appointment/VP9p5LWppKj4dzm/pay/ (Validation time only 6 minutes)
